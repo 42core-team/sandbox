5a41ca05cb241c80366c305a071fec9d8a0a25e9
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-02-23 19:31:30 +0100

    Für Jonas Götz

--- bot/src/main.c
+++ bot/src/main.c
@@ -16,4 +16,35 @@ void	ft_user_loop(void)
 {
 	// your actions go here
 	ft_print_all();
+
+	static int count = 0;
+	if (count == 0)
+	{
+		ft_create(&game.config.unit_configs[1]);
+		count++;
+	}
+	int ind = 0;
+	t_core *core;
+	while (game.cores[ind].id)
+	{
+		if (game.cores[ind].team_id != game.team_id)
+		{
+			core = &game.cores[ind];
+			break;
+		}
+		ind++;
+	}
+
+	ind = 0;
+	while (game.units[ind].id) {
+		// if (game.units[ind].team_id == game.team_id && game.resources[0].id) {
+		// 	ft_travel_to_id(game.units[ind].id, game.resources[0].x, game.resources[0].y);
+		// 	ft_attack_id(game.units[ind].id, game.resources[0].id);
+		// }
+		if (game.units[ind].team_id == game.team_id) {
+			ft_travel_to_id(game.units[ind].id, core->x, core->y);
+			ft_attack_id(game.units[ind].id, core->id);
+		}
+		ind++;
+	}
 }
