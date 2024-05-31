416d517c442ca964a6ab966322b338a56b16c8ae
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-05-31 21:48:59 +0200

    starlord: added back

---
+++ bot/starlord/src/main.c
@@ -0,0 +1,19 @@
+#include "con_lib.h"
+
+void	ft_user_loop(void);
+
+int	main(int argc, char **argv)
+{
+	// ft_enable_debug();
+	ft_init_con(&argc, argv);
+	ft_loop(&ft_user_loop);
+	ft_close_con();
+	return (0);
+}
+
+// this function is called every time new data is recieved
+void	ft_user_loop(void)
+{
+	// your actions go here
+	ft_print_all();
+}
