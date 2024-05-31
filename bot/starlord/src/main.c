c3262bb69e704f8d065eace2a1b647e6811b127b
Paulic <61714149+PaulicStudios@users.noreply.github.com>
2024-05-31 20:21:35 +0000

    starlord: first version

--- bot/starlord/src/main.c
+++ bot/starlord/src/main.c
@@ -1,4 +1,5 @@
 #include "con_lib.h"
+#include <stdio.h>
 
 void	ft_user_loop(void);
 
@@ -14,6 +15,36 @@ int	main(int argc, char **argv)
 // this function is called every time new data is recieved
 void	ft_user_loop(void)
 {
-	// your actions go here
-	ft_print_all();
+	t_core	*opponent_core;
+	t_team	*my_team;
+	int		ind;
+
+	ind = 0;
+	while (game.cores[ind].id != 0)
+	{
+		if (game.cores[ind].team_id != game.my_team_id)
+			opponent_core = &game.cores[ind];
+		ind++;
+	}
+
+	ind = 0;
+	while (game.teams[ind].id != 0)
+	{
+		if (game.teams[ind].id == game.my_team_id)
+			my_team = &game.teams[ind];
+		ind++;
+	}
+
+	if (my_team->balance >= game.config.units[0].cost)
+		ft_create_type_id(game.config.units[0].type_id);
+	ind = 0;
+	while (game.units[ind].id != 0)
+	{
+		if (game.units[ind].team_id == game.my_team_id)
+		{
+			ft_travel_to(&game.units[ind], opponent_core->x, opponent_core->y);
+			ft_attack_core(&game.units[ind], opponent_core);
+		}
+		ind++;
+	}
 }
