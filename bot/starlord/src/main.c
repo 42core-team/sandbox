602113f0f55f8676af1d109d64e505711158d9ea
Paul Großmann <git@paulgrossmann.de>
2025-01-31 22:55:12 +0100

    Reset Starlord to old simple version

--- bot/starlord/src/main.c
+++ bot/starlord/src/main.c
@@ -1,68 +1,32 @@
 #include "con_lib.h"
 
-#include <stdio.h>
-
 void	ft_user_loop(void *ptr);
 
 int	main(int argc, char **argv)
 {
 	// ft_enable_debug();
-	ft_init_con("Thanos", &argc, argv);
+	ft_init_con("Starlord", &argc, argv);
 	ft_loop(NULL, &ft_user_loop, NULL);
 	ft_close_con();
 	return (0);
 }
-
-int nextUnitType = 2;
 
 // this function is called every time new data is recieved
 void	ft_user_loop(void *ptr)
 {
 	(void) ptr;
 
-	ft_print_units();
+	if (ft_get_my_team()->balance >= ft_get_unit_config(UNIT_WARRIOR)->cost)
+		ft_create_unit(UNIT_WARRIOR);
 
-	t_obj **units = ft_get_my_units();
-	t_team *myTeam = ft_get_my_team();
-	if (!units || !myTeam)
-		return;
+	t_obj **my_units = ft_get_my_units();
 
-	if (ft_get_unit_config(nextUnitType)->cost <= myTeam->balance)
+	int ind = 0;
+	while (my_units[ind] != NULL)
 	{
-		ft_create_unit(nextUnitType);
-		nextUnitType++;
-		if (nextUnitType > 5)
-			nextUnitType = 1;
+		ft_travel_attack(my_units[ind], ft_get_first_opponent_core());
+		ind++;
 	}
 
-	int i = 0;
-	while (units && units[i] != 0)
-	{
-		t_obj *unit = units[i];
-		t_obj *nearestResource = ft_get_nearest_resource(unit);
-		t_obj *nearestOpponentUnit = ft_get_nearest_opponent_unit(unit);
-		t_obj *nearestOpponentCore = ft_get_first_opponent_core();
-
-		switch (units[i]->s_unit.type_id)
-		{
-			case UNIT_TANK:
-				ft_travel_attack(unit, nearestOpponentCore);
-				break;
-			case UNIT_WORKER:
-				ft_travel_attack(unit, nearestResource);
-				break;
-			case UNIT_HEALER:
-				if (nearestOpponentCore)
-					ft_travel_attack(unit, ft_get_nearest_team_unit(nearestOpponentCore));
-				break;
-			default:
-				if (nearestOpponentUnit != NULL)
-					ft_travel_attack(unit, nearestOpponentUnit);
-				else
-					ft_travel_attack(unit, nearestOpponentCore);
-				break;
-		}
-		i++;
-	}
-	free(units);
+	free(my_units);
 }
